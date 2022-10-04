8dd072a1f6d2b8e977bd5dae3ccc8242959e3a2c
Jacob Beffrey <jacob.beffrey@Gmail.com>
2022-10-04 09:05:48 -0400

    not finsished

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strick";
+
+const name = "Jacob beffrey"; // sting
+let age = 22; // number
+const birthday = "april 28"; // string
+let pineapplePizza = false; // boolean
+
+const lifeEvents = [
+  "I was boarn in battle creek michigan",
+  "I went to gull lake high school",
+  "I currently live in downtown Kalamazoo with my girlfirend and two friends",
+  "I enjoy golfing, bowling, skiing, playing vidoe games, and building legos",
+]; //array
+
+if (pineapplePizza === true) {
+  console.log(
+    `My name is name and I like pineapples on pizza. I am currently age years old and my birthday is on birthday.`
+  );
+}
